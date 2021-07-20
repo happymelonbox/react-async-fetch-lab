21a781d4c228bb7ad17ee6aa1a8da30dc67cc570
Justin Berger Howes <howes.j.j@gmail.com>
2021-07-20 11:12:51 +1000

    fetch working - rendering ugly though

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
-// create your App component here
+import React from 'react'
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            people: []
+        }
+    }
+
+    
+    render(){
+        return (
+            <ul>
+                <li>
+                    {this.state.people.map(person=>person.name)}
+                </li>
+            </ul>
+        )
+    }
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp=>resp.json())
+        .then(data=> {
+            this.setState({
+                people: data.people
+            })
+        })
+    }
+}
+
+
+export default App
